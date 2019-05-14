3eaf495142498d05c36cfc0cdaf2c71cb77d7b1c
ronraisch <33007562+ronraisch@users.noreply.github.com>
2019-05-14 14:43:39 +0300

    Update board.py
    added new functions that are needed for mutation

--- board.py
+++ board.py
@@ -1,11 +1,8 @@
-import buildings
-import army
-
 class GameBoard(object):
 
     BOARD_SIZE = 20
     BUILDING_DICT = {
-        0 :
+
     }
 
 
@@ -13,6 +10,18 @@ class GameBoard(object):
         self._board = []
         for i in range(GameBoard.BOARD_SIZE):
             self._board.append([0] * GameBoard.BOARD_SIZE)
+    
+    def get_empty_positions(self):
+        return 0
+    
+    def get_empty_buildings(self):
+        return 0
+    
+    def get_walls_positions(self):
+        return 0
+    
+    def get_buildings_positions(self):
+        return 0
 
     def get_board(self):
         return self._board
