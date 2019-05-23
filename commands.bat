3e20e342f047ad707077e40749c3c5d09faaddf3
ronraisch <33007562+ronraisch@users.noreply.github.com>
2019-05-23 23:53:37 +0300

    Create commands.bat

---
+++ commands.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/ronraisch/Clash-of-Clans-AI.git
+cd Clash-of-Clans-AI
+For %%p IN (where python) DO set PATH=%PATH%;%%p
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python -m http.server 8000
+python runGenetics.py
