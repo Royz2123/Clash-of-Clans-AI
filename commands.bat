bfae8d11e712e417abebc67cf7389accbff9f76b
ronraisch <33007562+ronraisch@users.noreply.github.com>
2019-05-24 15:14:10 +0300

    added python to path correctly

--- commands.bat
+++ commands.bat
@@ -1,7 +1,7 @@
 color 0a
 git clone https://github.com/ronraisch/Clash-of-Clans-AI.git
 cd Clash-of-Clans-AI
-For %%p IN (where python) DO set PATH=%PATH%;%%p
+For /f %%p IN ('where python') DO set PATH=%PATH%;%%p
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m http.server 8000
