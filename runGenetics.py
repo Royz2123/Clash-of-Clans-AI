99003fce37139b524a212702274daf3bc9f10fb1
ronraisch <33007562+ronraisch@users.noreply.github.com>
2019-05-25 19:31:23 +0300

    now running on linux

--- runGenetics.py
+++ runGenetics.py
@@ -1,65 +1,65 @@
-from pyeasyga import pyeasyga
+import pyeasyga.pyeasyga as ps
 from GameGenetics import *
 
+if __name__ == "__main__":
 
-seed_data = GameGenetics()
-print("seed")
-# initialise the GA
-ga = pyeasyga.GeneticAlgorithm(seed_data,
-                               population_size=300,
-                               generations=200,
-                               mutation_probability=MUTATION_RATE,
-                               elitism=True,
-                               maximise_fitness=True)
-
-
-# define and set function to create a candidate solution representation
-def create_individual(data):
-    return data.create_individual()
+    seed_data = GameGenetics()
+    # initialise the GA
+    ga = ps.GeneticAlgorithm(seed_data,
+                                   population_size=50,
+                                   generations=200,
+                                   mutation_probability=MUTATION_RATE,
+                                   elitism=True,
+                                   maximise_fitness=False)
 
 
-ga.create_individual = create_individual
+    # define and set function to create a candidate solution representation
+    def create_individual(data):
+        return data.create_individual()
 
 
-# define and set the GA's crossover operation
-def crossover(parent_1, parent_2):
-    return parent_1,parent_2
-
-ga.crossover_function = crossover
-
-# define and set the GA's mutation operation
-def mutate(individual):
-    individual.mutation(MUTATION_CHANGE)
-    return individual
-
-ga.mutate_function = mutate
+    ga.create_individual = create_individual
 
 
-# define and set the GA's selection operation
-def selection(population):
-    r = random.random()
-    fitness_list = [element.genes.get_fitness() for element in population]
-    sum_of_fitness = sum(fitness_list)
-    probs = [fit / sum_of_fitness for fit in fitness_list]
-    i = 0
-    while r > 0:
-        r -= probs[i]
-    return population[i]
+    # define and set the GA's crossover operation
+    def crossover(parent_1, parent_2):
+        return parent_1,parent_2
+
+    ga.crossover_function = crossover
+
+    # define and set the GA's mutation operation
+    def mutate(individual):
+        individual.mutation(MUTATION_CHANGE)
+        return individual
+
+    ga.mutate_function = mutate
 
 
-ga.selection_function = selection
+    # define and set the GA's selection operation
+    def selection(population):
+        r = random.random()
+        fitness_list = [element.genes.get_fitness() for element in population]
+        sum_of_fitness = sum(fitness_list)
+        probs = [fit / sum_of_fitness for fit in fitness_list]
+        i = 0
+        while r > 0:
+            r -= probs[i]
+        return population[i]
 
 
-# define a fitness function
-def fitness(individual, data):
-    return individual.get_fitness()
+    ga.selection_function = selection
 
 
-ga.fitness_function = fitness  # set the GA's fitness function
+    # define a fitness function
+    def fitness(individual, data):
+        return individual.get_fitness()
 
 
-ga.run()  # run the GA
+    ga.fitness_function = fitness  # set the GA's fitness function
 
 
-ga.best_individual()[1].game_board.update_viz()
+    ga.run()  # run the GA
+
+
+    ga.best_individual()[1].game_board.update_viz()
 
